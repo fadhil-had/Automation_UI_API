d2360e27ea5111285368d5efb8ff4102ef82443f
fadhil-had <fadsyahad@gmail.com>
2025-09-24 23:38:25 +0700

    Update driver factory file and folder

---
+++ src/main/java/utils/DriverFactory.java
@@ -0,0 +1,29 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class DriverFactory {
+    private static AppiumDriver driver;
+
+    public static AppiumDriver getDriver() throws MalformedURLException {
+        if (driver == null){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("automationName", "UiAutomator2");
+            caps.setCapability("deviceName", "emulator-5554"); //Please replace it with your device name
+
+            // APK yang disimpan di folder "app/"
+            caps.setCapability("app", System.getProperty("user.dir") + "/app/mda-1.0.13-15.apk");
+
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+        }
+        return driver;
+    }
+}
